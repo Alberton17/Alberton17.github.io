29a8af08e02817712ac54c1d6e10ee4b94f75790
Alberton17 <159498717+Alberton17@users.noreply.github.com>
2024-11-04 22:26:57 -0800

    Practica 8

---
+++ Parcial2/practica8.js
@@ -0,0 +1,12 @@
+$(document).ready( function(){
+    alert("kill yourself");
+
+    $("#but").click(function(){
+        var name = $("#nombre").val();
+        alert(name);
+    });
+
+    $("#texto").click(function(){
+        $("#texto").css("display", "inherit").fadeOut(200);
+    });
+});
